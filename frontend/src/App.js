bd606f1c943f780d6495607debf5291ab25eceba
izar1991 <izarvillasante@gmail.com>
2025-08-13 05:45:30 +0200

    Add App component for frontend

---
+++ frontend/src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Expenses from "./Expenses";
+import Accountability from "./Accountability";
+
+function App() {
+  return (
+    <div>
+      <h1>Family Clinic App</h1>
+      <Expenses />
+      <Accountability />
+    </div>
+  );
+}
+
+export default App;
